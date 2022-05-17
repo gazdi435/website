54519ec567811ea7524ec4359c11623a19350f67
gazdi435 <72067999+gazdi435@users.noreply.github.com>
2022-05-17 19:57:11 +0200

    Add files via upload

---
+++ weboldal/proba.js
@@ -0,0 +1,298 @@
+function elsogomb() {
+    document.getElementById("elsőszöveg").innerHTML = "Tear of goddess rush";
+    document.getElementById("másodikszöveg").innerHTML = "Jó lenne idegenvezetőnek";
+    document.getElementById("harmadikszöveg").innerHTML = "Q spam";
+    document.getElementById("negyedikszöveg").innerHTML = "Jarro Lightfeather";
+    document.getElementById("ötödikszöveg").innerHTML = "It's okey to be gay";
+    document.getElementById("megoldás").innerHTML = "Ezreal";
+    document.getElementById('elsőszöveg').style.cssText = 'color: rgba(0, 0, 0, 0)';
+    document.getElementById('másodikszöveg').style.cssText = 'color: rgba(0, 0, 0, 0)';
+    document.getElementById('harmadikszöveg').style.cssText = 'color: rgba(0, 0, 0, 0)';
+    document.getElementById('negyedikszöveg').style.cssText = 'color: rgba(0, 0, 0, 0)';
+    document.getElementById('ötödikszöveg').style.cssText = 'color: rgba(0, 0, 0, 0)';
+    document.getElementById('megoldás').style.cssText = 'color: rgba(0, 0, 0, 0); text-shadow: none';
+    document.getElementById('képharmadik').src='';
+
+}
+
+function masodikgomb() {
+    document.getElementById("elsőszöveg").innerHTML = "Hozzá képest darius skilles champ";
+    document.getElementById("másodikszöveg").innerHTML = "Ó rómeó...";
+    document.getElementById("harmadikszöveg").innerHTML = "Koronaőr";
+    document.getElementById("negyedikszöveg").innerHTML = "What are you doing step brother?";
+    document.getElementById("ötödikszöveg").innerHTML = "Spin to win";
+    document.getElementById("megoldás").innerHTML = "Garen";
+    document.getElementById('elsőszöveg').style.cssText = 'color: rgba(0, 0, 0, 0)';
+    document.getElementById('másodikszöveg').style.cssText = 'color: rgba(0, 0, 0, 0)';
+    document.getElementById('harmadikszöveg').style.cssText = 'color: rgba(0, 0, 0, 0)';
+    document.getElementById('negyedikszöveg').style.cssText = 'color: rgba(0, 0, 0, 0)';
+    document.getElementById('ötödikszöveg').style.cssText = 'color: rgba(0, 0, 0, 0)';
+    document.getElementById('megoldás').style.cssText = 'color: rgba(0, 0, 0, 0); text-shadow: none';
+    document.getElementById('képharmadik').src='';
+}
+
+function harmadikgomb() {
+    document.getElementById("elsőszöveg").innerHTML = "Ha ellene játszol, akkor király vagy";
+    document.getElementById("másodikszöveg").innerHTML = "Amit viego meghagy";
+    document.getElementById("harmadikszöveg").innerHTML = "\"You dare to use my own spell against me?!\"";
+    document.getElementById('képharmadik').src='Képek/sylas1.jfif'
+    document.getElementById("negyedikszöveg").innerHTML = "Little light";
+    document.getElementById("ötödikszöveg").innerHTML = "Our ult";
+    document.getElementById("megoldás").innerHTML = "Sylas";
+    document.getElementById('elsőszöveg').style.cssText = 'color: rgba(0, 0, 0, 0)';
+    document.getElementById('másodikszöveg').style.cssText = 'color: rgba(0, 0, 0, 0)';
+    document.getElementById('harmadikszöveg').style.cssText = 'color: rgba(0, 0, 0, 0)';
+    document.getElementById('negyedikszöveg').style.cssText = 'color: rgba(0, 0, 0, 0)';
+    document.getElementById('ötödikszöveg').style.cssText = 'color: rgba(0, 0, 0, 0)';
+    document.getElementById('megoldás').style.cssText = 'color: rgba(0, 0, 0, 0); text-shadow: none';
+    document.getElementById('képharmadik').style.width ='200px'
+    document.getElementById('képharmadik').style.margin ='15px';
+}
+
+function negyedikgomb() {
+    document.getElementById("elsőszöveg").innerHTML = "Úgysem fog mandettát venni";
+    document.getElementById("másodikszöveg").innerHTML = "Light 'em up";
+    document.getElementById("harmadikszöveg").innerHTML = "Az előző három champ nagy hint";
+    document.getElementById("negyedikszöveg").innerHTML = "Sismacia";
+    document.getElementById("ötödikszöveg").innerHTML = "\"Hova fut a mundó?!\"";
+    document.getElementById("megoldás").innerHTML = "Lux";
+    document.getElementById('elsőszöveg').style.cssText = 'color: rgba(0, 0, 0, 0)';
+    document.getElementById('másodikszöveg').style.cssText = 'color: rgba(0, 0, 0, 0)';
+    document.getElementById('harmadikszöveg').style.cssText = 'color: rgba(0, 0, 0, 0)';
+    document.getElementById('negyedikszöveg').style.cssText = 'color: rgba(0, 0, 0, 0)';
+    document.getElementById('ötödikszöveg').style.cssText = 'color: rgba(0, 0, 0, 0)';
+    document.getElementById('megoldás').style.cssText = 'color: rgba(0, 0, 0, 0); text-shadow: none';
+    document.getElementById('képharmadik').src='';
+}
+
+function ötödikgomb() {
+    document.getElementById("elsőszöveg").innerHTML = "\"Asszem nemrég jött ki\"";
+    document.getElementById("másodikszöveg").innerHTML = "unmuted";
+    document.getElementById("harmadikszöveg").innerHTML = "Oneshotol support itemmel (imádlak riot <3)";
+    document.getElementById("negyedikszöveg").innerHTML = "Meg fog táncoltatni";
+    document.getElementById("ötödikszöveg").innerHTML = "djsona2010";
+    document.getElementById("megoldás").innerHTML = "Sona";
+    document.getElementById('elsőszöveg').style.cssText = 'color: rgba(0, 0, 0, 0)';
+    document.getElementById('másodikszöveg').style.cssText = 'color: rgba(0, 0, 0, 0)';
+    document.getElementById('harmadikszöveg').style.cssText = 'color: rgba(0, 0, 0, 0)';
+    document.getElementById('negyedikszöveg').style.cssText = 'color: rgba(0, 0, 0, 0)';
+    document.getElementById('ötödikszöveg').style.cssText = 'color: rgba(0, 0, 0, 0)';
+    document.getElementById('megoldás').style.cssText = 'color: rgba(0, 0, 0, 0); text-shadow: none';
+    document.getElementById('képharmadik').src='';
+}
+
+function hatodikgomb() {
+    document.getElementById("elsőszöveg").innerHTML = "\"Adc\"";
+    document.getElementById("másodikszöveg").innerHTML = "\"Nincs ultija\"";
+    document.getElementById("harmadikszöveg").innerHTML = "Send me nukes";
+    document.getElementById("negyedikszöveg").innerHTML = "Mid or nuke";
+    document.getElementById("ötödikszöveg").innerHTML = "Allah akbar";
+    document.getElementById("megoldás").innerHTML = "Ziggs";
+    document.getElementById('elsőszöveg').style.cssText = 'color: rgba(0, 0, 0, 0)';
+    document.getElementById('másodikszöveg').style.cssText = 'color: rgba(0, 0, 0, 0)';
+    document.getElementById('harmadikszöveg').style.cssText = 'color: rgba(0, 0, 0, 0)';
+    document.getElementById('negyedikszöveg').style.cssText = 'color: rgba(0, 0, 0, 0)';
+    document.getElementById('ötödikszöveg').style.cssText = 'color: rgba(0, 0, 0, 0)';
+    document.getElementById('megoldás').style.cssText = 'color: rgba(0, 0, 0, 0); text-shadow: none';
+    document.getElementById('képharmadik').src='';
+}
+
+function hetedikgomb() {
+    document.getElementById("elsőszöveg").innerHTML = "Boba Fett";
+    document.getElementById("másodikszöveg").innerHTML = "Az arcom counterli";
+    document.getElementById("harmadikszöveg").innerHTML = "Why so serious?";
+    document.getElementById("negyedikszöveg").innerHTML = "Szereted a bohócokat";
+    document.getElementById("ötödikszöveg").innerHTML = "Pinkward";
+    document.getElementById("megoldás").innerHTML = "Shaco";
+    document.getElementById('elsőszöveg').style.cssText = 'color: rgba(0, 0, 0, 0)';
+    document.getElementById('másodikszöveg').style.cssText = 'color: rgba(0, 0, 0, 0)';
+    document.getElementById('harmadikszöveg').style.cssText = 'color: rgba(0, 0, 0, 0)';
+    document.getElementById('negyedikszöveg').style.cssText = 'color: rgba(0, 0, 0, 0)';
+    document.getElementById('ötödikszöveg').style.cssText = 'color: rgba(0, 0, 0, 0)';
+    document.getElementById('megoldás').style.cssText = 'color: rgba(0, 0, 0, 0); text-shadow: none';
+    document.getElementById('képharmadik').src='';
+}
+
+function nyolcadikgomb() {
+    document.getElementById("elsőszöveg").innerHTML = "Le fog pókolni";
+    document.getElementById("másodikszöveg").innerHTML = "Dive at lvl 3";
+    document.getElementById("harmadikszöveg").innerHTML = "Húkusz pókusz";
+    document.getElementById("negyedikszöveg").innerHTML = "Sok guide van róla a weben";
+    document.getElementById("ötödikszöveg").innerHTML = "Earlyben szövi a terveit";
+    document.getElementById("megoldás").innerHTML = "Elise";
+    document.getElementById('elsőszöveg').style.cssText = 'color: rgba(0, 0, 0, 0)';
+    document.getElementById('másodikszöveg').style.cssText = 'color: rgba(0, 0, 0, 0)';
+    document.getElementById('harmadikszöveg').style.cssText = 'color: rgba(0, 0, 0, 0)';
+    document.getElementById('negyedikszöveg').style.cssText = 'color: rgba(0, 0, 0, 0)';
+    document.getElementById('ötödikszöveg').style.cssText = 'color: rgba(0, 0, 0, 0)';
+    document.getElementById('megoldás').style.cssText = 'color: rgba(0, 0, 0, 0); text-shadow: none';
+    document.getElementById('képharmadik').src='';
+}
+
+function hint1() {
+    document.getElementById('elsőszöveg').style.cssText = 'background-color: none; color: white';
+}
+
+function hint2() {
+    document.getElementById('másodikszöveg').style.cssText = 'background-color: none; color: white';
+}
+
+function hint3() {
+    document.getElementById('harmadikszöveg').style.cssText = 'background-color: none; color: white';
+}
+
+function hint4() {
+    document.getElementById('negyedikszöveg').style.cssText = 'background-color: none; color: white';
+}
+
+function hint5() {
+    document.getElementById('ötödikszöveg').style.cssText = 'background-color: none; color: white';
+}
+
+function hint6() {
+    document.getElementById('megoldás').style.cssText = 'background-color: none; color: green';
+}
+
+function kivagyokelsőgomb() {
+    document.getElementById('kivagyokhint').innerHTML = "\"A sivatagi rózsa\"";
+    document.getElementById('kivagyokválasz').innerHTML = "Samira";
+    document.getElementById('kivagyokválasz').style.cssText = 'color: rgba(0, 0, 0, 0)';
+}
+
+function kivagyokmásodikgomb() {
+    document.getElementById('kivagyokhint').innerHTML = "\"A vasszűz\"";
+    document.getElementById('kivagyokválasz').innerHTML = "Rell";
+    document.getElementById('kivagyokválasz').style.cssText = 'color: rgba(0, 0, 0, 0)';
+}
+
+function kivagyokharmadikgomb() {
+    document.getElementById('kivagyokhint').innerHTML = "\"Az idő őre\"";
+    document.getElementById('kivagyokválasz').innerHTML = "Zilean";
+    document.getElementById('kivagyokválasz').style.cssText = 'color: rgba(0, 0, 0, 0)';
+}
+
+function kivagyoknegyedikgomb() {
+    document.getElementById('kivagyokhint').innerHTML = "\"Az éji vadász\"";
+    document.getElementById('kivagyokválasz').innerHTML = "Vayne";
+    document.getElementById('kivagyokválasz').style.cssText = 'color: rgba(0, 0, 0, 0)';
+}
+
+function kivagyokötödikgomb() {
+    document.getElementById('kivagyokhint').innerHTML = "\"Észak dühe\"";
+    document.getElementById('kivagyokválasz').innerHTML = "Sejuani";
+    document.getElementById('kivagyokválasz').style.cssText = 'color: rgba(0, 0, 0, 0)';
+}
+
+function kivagyokhatodikgomb() {
+    document.getElementById('kivagyokhint').innerHTML = "\"Demacia szárnyai\"";
+    document.getElementById('kivagyokválasz').innerHTML = "Quinn";
+    document.getElementById('kivagyokválasz').style.cssText = 'color: rgba(0, 0, 0, 0)';
+}
+
+function kivagyokhetedikgomb() {
+    document.getElementById('kivagyokhint').innerHTML = "\"A nagy párbajhős\"";
+    document.getElementById('kivagyokválasz').innerHTML = "Fiora";
+    document.getElementById('kivagyokválasz').style.cssText = 'color: rgba(0, 0, 0, 0)';
+}
+
+function kivagyoknyolcadikgomb() {
+    document.getElementById('kivagyokhint').innerHTML = "\"Demacia eszménye\"";
+    document.getElementById('kivagyokválasz').innerHTML = "Jarvan IV";
+    document.getElementById('kivagyokválasz').style.cssText = 'color: rgba(0, 0, 0, 0)';
+}
+
+function kivagyokkilencedikgomb() {
+    document.getElementById('kivagyokhint').innerHTML = "\"A zsivány orgyilkos\"";
+    document.getElementById('kivagyokválasz').innerHTML = "Akali";
+    document.getElementById('kivagyokválasz').style.cssText = 'color: rgba(0, 0, 0, 0)';
+}
+
+function kivagyoktizedikgomb() {
+    document.getElementById('kivagyokhint').innerHTML = "\"A hatalmas ent\"";
+    document.getElementById('kivagyokválasz').innerHTML = "Maokai";
+    document.getElementById('kivagyokválasz').style.cssText = 'color: white';
+}
+
+function kivagyok11() {
+    document.getElementById('kivagyokhint').innerHTML = "\"A vakmerő tüzér\"";
+    document.getElementById('kivagyokválasz').innerHTML = "Corki";
+    document.getElementById('kivagyokválasz').style.cssText = 'color: rgba(0, 0, 0, 0)';
+}
+
+function kivagyok12() {
+    document.getElementById('kivagyokhint').innerHTML = "\"Az üresség réme\"";
+    document.getElementById('kivagyokválasz').innerHTML = "Cho'gath";
+    document.getElementById('kivagyokválasz').style.cssText = 'color: rgba(0, 0, 0, 0)';
+}
+
+function kivagyokhint() {
+    document.getElementById('kivagyokválasz').style.cssText = 'color: rgba(0, 0, 0, 0)';
+}
+
+function skincompválasz() {
+    document.getElementById('skincompvalasz').style.cssText = 'color: white';
+}
+
+function skincompelsőgomb() {
+    document.getElementById('skincompelsohint').innerHTML = "Kennen";
+    document.getElementById('skincompmasodik').innerHTML = "Thresh";
+    document.getElementById('skincompharmadik').innerHTML = "Shen";
+    document.getElementById('skincompvalasz').innerHTML = "Blood moon";
+    document.getElementById('skincompvalasz').style.cssText = 'color: rgba(0, 0, 0, 0)';
+}
+
+function skincompmásodikgomb() {
+    document.getElementById('skincompelsohint').innerHTML = "Nasus";
+    document.getElementById('skincompmasodik').innerHTML = "Cassiopeia";
+    document.getElementById('skincompharmadik').innerHTML = "Warwick";
+    document.getElementById('skincompvalasz').innerHTML = "Lunar level";
+    document.getElementById('skincompvalasz').style.cssText = 'color: rgba(0, 0, 0, 0)';
+}
+
+function skincompharmadikgomb() {
+    document.getElementById('skincompelsohint').innerHTML = "Cho'gath";
+    document.getElementById('skincompmasodik').innerHTML = "Vel'koz";
+    document.getElementById('skincompharmadik').innerHTML = "Xerath";
+    document.getElementById('skincompvalasz').innerHTML = "Battlecast";
+    document.getElementById('skincompvalasz').style.cssText = 'color: rgba(0, 0, 0, 0)';
+}
+
+function skincompnegyedikgomb() {
+    document.getElementById('skincompelsohint').innerHTML = "Pyke";
+    document.getElementById('skincompmasodik').innerHTML = "Kayle";
+    document.getElementById('skincompharmadik').innerHTML = "Viktor";
+    document.getElementById('skincompvalasz').innerHTML = "Psyops";
+    document.getElementById('skincompvalasz').style.cssText = 'color: rgba(0, 0, 0, 0)';
+}
+
+function skincompotodikgomb() {
+    document.getElementById('skincompelsohint').innerHTML = "Hekarim";
+    document.getElementById('skincompmasodik').innerHTML = "Ahri";
+    document.getElementById('skincompharmadik').innerHTML = "Veigar";
+    document.getElementById('skincompvalasz').innerHTML = "Elderwood";
+    document.getElementById('skincompvalasz').style.cssText = 'color: rgba(0, 0, 0, 0)';
+}
+
+function skincomphatodikgomb() {
+    document.getElementById('skincompelsohint').innerHTML = "Alistar";
+    document.getElementById('skincompmasodik').innerHTML = "Diana";
+    document.getElementById('skincompharmadik').innerHTML = "Karthus";
+    document.getElementById('skincompvalasz').innerHTML = "Infernal";
+    document.getElementById('skincompvalasz').style.cssText = 'color: rgba(0, 0, 0, 0)';
+}
+
+function skincomphetedikgomb() {
+    document.getElementById('skincompelsohint').innerHTML = "Ashe";
+    document.getElementById('skincompmasodik').innerHTML = "Rammus";
+    document.getElementById('skincompharmadik').innerHTML = "Taliyah";
+    document.getElementById('skincompvalasz').innerHTML = "Freljord";
+    document.getElementById('skincompvalasz').style.cssText = 'color: rgba(0, 0, 0, 0)';
+}
+
+function skincompnyolcadikgomb() {
+    document.getElementById('skincompelsohint').innerHTML = "Braum";
+    document.getElementById('skincompmasodik').innerHTML = "Draven";
+    document.getElementById('skincompharmadik').innerHTML = "Gragas";
+    document.getElementById('skincompvalasz').innerHTML = "Santa";
+    document.getElementById('skincompvalasz').style.cssText = 'color: rgba(0, 0, 0, 0)';
+}
